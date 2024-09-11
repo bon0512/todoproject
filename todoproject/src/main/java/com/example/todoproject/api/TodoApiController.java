a2e89f2d61abf0c589a5ab7805ff7f74003dd4c7
bon0512 <nonstop00450@gmail.com>
2024-09-11 22:32:43 +0900

    간단한 REST API 구현

--- todoproject/src/main/java/com/example/todoproject/api/TodoApiController.java
+++ todoproject/src/main/java/com/example/todoproject/api/TodoApiController.java
@@ -1,11 +1,13 @@
 package com.example.todoproject.api;
 
 
+import com.example.todoproject.dto.TodoForm;
 import com.example.todoproject.entity.Todo;
 import com.example.todoproject.repository.TodoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -26,8 +28,40 @@ public class TodoApiController {
 
 
     //POST 추가
+    @PostMapping("/api/createtodo")
+    public Todo create(@RequestBody TodoForm dto){
+
+        Long maxOrderNumber = todoRepository.findMaxOrderNumber();
+
+        Long newOrderNumber = (maxOrderNumber !=null)?maxOrderNumber+1:1;
+
+        Todo todo = dto.toEntity(newOrderNumber);
+
+        return todoRepository.save(todo);
+    }
+
 
     //DELETE 삭제
+
+    @DeleteMapping("/api/todo/{id}")
+    public ResponseEntity<Todo> delete(@PathVariable("id") Long id ){
+        //1.DB에서 대상 엔티티가 있는지 조회하기
+
+        Todo target = todoRepository.findById(id).orElse(null);
+
+
+        //2. 대상 엔티티가 없어서 요청 자체가 잘못됐을경우 처리하기
+        if(target==null){
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(null);
+        }
+
+
+        //3. 대상엔티티가 있으면 삭제하고 정상응답 반환하기
+
+        todoRepository.delete(target);
+        return ResponseEntity.status(HttpStatus.OK).build();
+
+    }
 
 
 }
--- todoproject/src/main/java/com/example/todoproject/dto/TodoForm.java
+++ todoproject/src/main/java/com/example/todoproject/dto/TodoForm.java
@@ -1,13 +1,23 @@
 package com.example.todoproject.dto;
 
 import com.example.todoproject.entity.Todo;
+import lombok.Getter;
+import lombok.Setter;
 
+
+@Getter
+@Setter
 public class TodoForm {
     private String content; //할일을 받을 필드
 
+
+    public TodoForm(){
+
+    }
     public TodoForm(String content) {
         this.content = content;
     }
+
 
     @Override
     public String toString() {
